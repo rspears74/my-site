1a27e8ac10274c34c2f6e6dfa680f90ba2fa098f
Randall Spears <rspears690@gmail.com>
2016-08-11 00:42:33 -0400

    initial

---
+++ simple-api.py
@@ -0,0 +1,86 @@
+from flask import Flask, request, render_template, redirect, url_for,\
+flash, session, send_file
+from flask_wtf import Form
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired
+from functools import wraps
+import hue
+
+
+app = Flask(__name__)
+app.secret_key = '0bq20g8n20nvq02nv0qjf09j203fj'
+
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Login required')
+            return redirect(url_for('login'))
+    return wrap
+
+
+@app.route('/lights/<on_off>')
+@login_required
+def lights_off(on_off):
+    lights = hue.get_all_lights()
+    if on_off=='on':
+        hue.set_all_lights_state(lights, True, hue.nice_yellow)
+        return "Lights on."
+    elif on_off=='off':
+        hue.set_all_lights_state(lights, False, hue.nice_yellow)
+        return "Lights off."
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    form = LoginForm(request.form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            if request.form['username'] == 'randall'\
+                and request.form['password'] == 'randall':
+                session['logged_in'] = True
+                flash('Successfully logged in.')
+                return redirect(url_for('home'))
+            else:
+                error = 'Invalid credentials'
+    return render_template('login.html', form=form, error=error)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    flash('Logged out')
+    return redirect(url_for('home'))
+
+
+
+@app.route('/camera')
+@login_required
+def camera():
+    return render_template('camera.html')
+
+
+class LoginForm(Form):
+
+    username = StringField(
+        'username',
+        validators=[DataRequired()]
+    )
+    password = PasswordField(
+        'password',
+        validators=[DataRequired()]
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
