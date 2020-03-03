8e669cd2ae36e31c5c18be31b7d2c98e1cb1c8aa
Randall Spears <rspears690@gmail.com>
2020-03-03 18:41:29 +0000

    removed most functionality

--- run.py
+++ run.py
@@ -1,19 +1,19 @@
 from app import app
-from home.hue import hue
-from home.views import home_ctrl
-from start.views import start
+#from home.hue import hue
+#from home.views import home_ctrl
+#from start.views import start
 from personal.views import personal
-from music.views import music
-from eng.views import eng
-from eng.api import eng_api
+#from music.views import music
+#from eng.views import eng
+#from eng.api import eng_api
 
-app.register_blueprint(hue.blueprint)
-app.register_blueprint(home_ctrl)
-app.register_blueprint(start)
+#app.register_blueprint(hue.blueprint)
+#app.register_blueprint(home_ctrl)
+#app.register_blueprint(start)
 app.register_blueprint(personal)
-app.register_blueprint(music)
-app.register_blueprint(eng_api.blueprint, url_prefix='/api')
-app.register_blueprint(eng)
+#app.register_blueprint(music)
+#app.register_blueprint(eng_api.blueprint, url_prefix='/api')
+#app.register_blueprint(eng)
 
 if __name__ == '__main__':
     app.run()
